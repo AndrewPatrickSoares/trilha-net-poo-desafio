3c2f47848e2711274576a6b815d688b4dee3dbab
Andrew Soares <andrewsoares07@outlook.com>
2024-01-02 09:05:06 -0300

    Realizando testes com as classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smatphone Nokia:");
+Smartphone smartNokia1 = new Nokia(numero: "920104114", modelo: "Modelo: 456", imei: "3669", memoria: 32);
+smartNokia1.Ligar();
+smartNokia1.InstalarAplicativo("SNAP");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smatphone Iphone");
+Smartphone smartIphone1 = new Iphone(numero: "920243265", modelo: "Modelo: 588", imei: "4787", memoria: 32);
+smartIphone1.ReceberLigacao();
+smartIphone1.InstalarAplicativo("WhatsApp");
